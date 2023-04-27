e19e754d1c5cf427fd6584435eace2a81767ae0f
Parichat <ppleparichat@gmail.com>
2023-04-27 18:42:26 +0200

    Add Scroll indicator and Top button in JS

--- app.js
+++ app.js
@@ -14,3 +14,39 @@ closeBtn.addEventListener("click", ()=>{
 })
 
 
+const progressEl = document.querySelector(".progress")
+window.onscroll = () => scrollProgress();
+
+function scrollProgress() {
+  const pageHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const scrollTop = document.documentElement.scrollTop
+  const percentage = (scrollTop / pageHeight) * 100
+
+  progressEl.style.visibility = 'visible'
+
+  progressEl.style.width = percentage+"%"
+}
+
+const scrollBtn = document.querySelector(".top");
+const rootEl = document.documentElement;
+
+document.addEventListener("scroll", showBtn);
+scrollBtn.addEventListener("click", scrollToTopFunc);
+
+function showBtn() {
+  let pageHeight = rootEl.scrollHeight - rootEl.clientHeight;
+  let result = rootEl.scrollTop / pageHeight;
+
+  if (result > 0.3) {
+    scrollBtn.classList.add("showBtn");
+  } else {
+    scrollBtn.classList.remove("showBtn");
+  }
+}
+
+function scrollToTopFunc() {
+  rootEl.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
